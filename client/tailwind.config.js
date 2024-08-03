4a7bafd11e4c542aeedad2d71773d7a3d12f192c
suvam <suvampravu@gmail.com>
2024-08-03 23:18:02 +0530

    google fonts are ready to go

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6528F7",
+        sec: "#40128B",
+        theme: "#FF204E",
+        btn: "#FF004D",
+      },
+      fontFamily: {
+        sans: ['"Nunito Sans"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
