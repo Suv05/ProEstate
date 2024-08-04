766395a58dc0a94e39c0bd97fec04665b6ffa179
suvam <suvampravu@gmail.com>
2024-08-04 20:35:22 +0530

    update index.js

--- API/index.js
+++ API/index.js
@@ -3,15 +3,21 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-  console.log("conneted to db");
-}).catch((err)=>{
-  console.log(err);
-  
-})
+import userRouter from "./routes/user.route.js";
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("conneted to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`);
 });
+
+app.use("/api/user", userRouter);
