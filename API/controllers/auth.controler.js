59fea2009e460c3a07a97878c44a3d6ec21fcf66
suvam <suvampravu@gmail.com>
2024-08-05 11:03:14 +0530

    hashed password

--- API/controllers/auth.controler.js
+++ API/controllers/auth.controler.js
@@ -1,16 +1,21 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   console.log(req.body); // Debug: Log the request body
 
-  
   const { username, email, password } = req.body;
 
   try {
+    //Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create the user with the hashed password
     const user = await User.create({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
     res.json({ message: "User created successfully", user });
   } catch (error) {
