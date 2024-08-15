823afec3c35373c046814f0da92a86478ebdc773
suvam <suvampravu@gmail.com>
2024-08-15 23:56:35 +0530

    adding signin route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,12 +12,14 @@ import "./index.css";
 import App from "./components/routes/App.jsx";
 import Home from "./components/pages/Home.jsx";
 import Signup from "./components/pages/Signup.jsx";
+import Signin from "./components/pages/Signin.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
       <Route path="signup" element={<Signup />} />
+      <Route path="signIn" element={<Signin />} />
     </Route>
   )
 );
