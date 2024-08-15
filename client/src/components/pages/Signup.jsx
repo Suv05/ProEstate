c8b1cdaeb720197cf068a4b045a690db153e7ecb
suvam <suvampravu@gmail.com>
2024-08-15 22:09:40 +0530

    signup functinality complete

--- client/src/components/pages/Signup.jsx
+++ client/src/components/pages/Signup.jsx
@@ -1,17 +1,34 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Signup() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (formData) => {
+    console.log(formData);
+
+    const res = await fetch("/api/v1/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 font-sans">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">
           Create an Account
         </h2>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
           <div>
             <label className="block text-gray-700">Name</label>
             <input
               type="text"
+              {...register("name")}
               className="mt-1 px-4 py-2 border rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-[#5F0F40]"
               placeholder="Your Name"
             />
@@ -22,6 +39,7 @@ function Signup() {
               type="email"
               className="mt-1 px-4 py-2 border rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-[#5F0F40]"
               placeholder="Your Email"
+              {...register("email")}
             />
           </div>
           <div>
@@ -30,6 +48,7 @@ function Signup() {
               type="password"
               className="mt-1 px-4 py-2 border rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-[#5F0F40]"
               placeholder="Your Password"
+              {...register("password")}
             />
           </div>
           <button
