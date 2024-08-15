fba0e07a03a5736c23163601840f381e23c39954
suvam <suvampravu@gmail.com>
2024-08-16 01:30:47 +0530

    complete sign in backend

--- API/controllers/auth.controler.js
+++ API/controllers/auth.controler.js
@@ -1,5 +1,14 @@
 import "express-async-errors";
 import { StatusCodes } from "http-status-codes";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+//for throw errors
+import NotFoundError from "../error/NotFoundError.js";
+import UnauthError from "../error/UnauthError.js";
 
 //imported user model
 import User from "../models/user.model.js";
@@ -9,7 +18,41 @@ export const signup = async (req, res, next) => {
   const user = await User.create({
     ...req.body,
   });
+  res.status(StatusCodes.CREATED).json({
+    message: "User created successfully",
+    user: {
+      name: user.name,
+    },
+  });
+};
+
+//login route
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const validUser = await User.findOne({ email });
+  if (!validUser) {
+    throw new NotFoundError("User not found!😔");
+  }
+
+  const validPassword = await bcryptjs.compare(password, validUser.password);
+  if (!validPassword) {
+    throw new UnauthError("Invalid candidentals 🙊", StatusCodes.UNAUTHORIZED);
+  }
+
+  // Remove password before sending the user object in the response
+  validUser.password = undefined;
+
+  //create token
+  const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "30d",
+  });
+
   res
-    .status(StatusCodes.CREATED)
-    .json({ message: "User created successfully", user });
+    .cookie("access_token", token, { httpOnly: true })
+    .status(StatusCodes.ACCEPTED)
+    .json({
+      message: "User logged in successfully",
+      validUser,
+    });
 };
--- API/routes/auth.route.js
+++ API/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controler.js";
+import { signup, signin } from "../controllers/auth.controler.js";
 
 const router = express.Router();
 
 router.route("/signup").post(signup);
+router.route("/signin").post(signin);
 
 export default router;
