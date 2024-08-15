3bd61726c1068dc4104f14544e267a53b16982e6
suvam <suvampravu@gmail.com>
2024-08-15 12:00:08 +0530

    changes in user route

--- API/routes/user.route.js
+++ API/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { test}  from "../controllers/user.controler.js";
+import { test } from "../controllers/user.controler.js";
 
 const router = express.Router();
 
-router.get("/test", test);
+router.route("/").get(test);
 
 export default router;
