11dd4397c1adbc0a1d76ec2c7078a1bd241beae4
suvam <suvampravu@gmail.com>
2024-08-17 01:51:55 +0530

    added firebase to project

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.OAUTH_API_KEY,
+  authDomain: "prostate-d2554.firebaseapp.com",
+  projectId: "prostate-d2554",
+  storageBucket: "prostate-d2554.appspot.com",
+  messagingSenderId: "987452278850",
+  appId: "1:987452278850:web:30fcddab8b6436294945db",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
