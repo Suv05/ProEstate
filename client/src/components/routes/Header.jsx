06342a3cb279a68112566e84704472b35c117b50
suvam <suvampravu@gmail.com>
2024-08-17 01:13:49 +0530

    link to login route

--- client/src/components/routes/Header.jsx
+++ client/src/components/routes/Header.jsx
@@ -37,9 +37,11 @@ function Header() {
 
         {/* Sign Up and Login buttons */}
         <div className="flex items-center space-x-4">
-          <button className="border-1 border-btn text-btn hover:bg-btn hover:text-white px-4 py-2 rounded-md active:scale-75 hover:scale-105">
-            Login
-          </button>
+          <Link to="signin">
+            <button className="border-1 border-btn text-btn hover:bg-btn hover:text-white px-4 py-2 rounded-md active:scale-75 hover:scale-105">
+              Login
+            </button>
+          </Link>
           <Link to="signup">
             <button className="bg-primary text-white px-4 py-2 rounded-md hover:bg-sec active:scale-75 hover:scale-105">
               Sign Up
