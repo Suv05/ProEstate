6ed2920f3f029a28261d7cd27cfce375c05f6270
suvam <suvampravu@gmail.com>
2024-08-17 19:30:47 +0530

    added to google signin function

--- API/controllers/auth.controler.js
+++ API/controllers/auth.controler.js
@@ -2,6 +2,7 @@ import "express-async-errors";
 import { StatusCodes } from "http-status-codes";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -66,4 +67,60 @@ export const signin = async (req, res, next) => {
       message: "User logged in successfully",
       validUser,
     });
+};
+
+//sign in with google....
+export const signinWithGoogle = async (req, res, next) => {
+  //destructure the data of user
+  const { name, email, photo } = req.body;
+
+  //if user alrady exists in database
+  const validUser = await User.findOne({ email });
+  if (validUser) {
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+
+    // Remove password before sending the user object in the response
+    validUser.password = undefined;
+
+    return res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(StatusCodes.ACCEPTED)
+      .json({
+        message: "User logged in successfully",
+        validUser,
+      });
+  } else {
+    //if user is not in database then
+
+    //1st generate a random password
+    const genPass = crypto.randomBytes(8).toString("hex"); // Generates a random password
+    const hashPass = bcryptjs.hashSync(genPass, 10); //hashed usong bcript
+
+    // Format the name to lowercase, remove spaces, and append a unique suffix
+    const processedName = name.toLowerCase().replace(/\s+/g, "");
+    const uniqueSuffix = Math.floor(100 + Math.random() * 900);
+    const formattedName = `${processedName}${uniqueSuffix}`;
+
+    // Store the new user in the database
+    const newUser = await User.create({
+      name: formattedName,
+      email: email,
+      password: hashPass,
+      avatar: photo,
+    });
+
+    // Generate token for the new user
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY);
+
+    // Remove password before sending the user object in the response
+    newUser.password = undefined;
+
+    return res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(StatusCodes.CREATED)
+      .json({
+        message: "User registered and logged in successfully",
+        newUser,
+      });
+  }
 };
