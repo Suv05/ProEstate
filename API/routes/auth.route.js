f6d3cf7c062948be6770dec1fa0d2257245fae12
suvam <suvampravu@gmail.com>
2024-08-17 19:32:27 +0530

    added google route configuration

--- API/routes/auth.route.js
+++ API/routes/auth.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { signup, signin } from "../controllers/auth.controler.js";
+import {
+  signup,
+  signin,
+  signinWithGoogle,
+} from "../controllers/auth.controler.js";
 
 const router = express.Router();
 
 router.route("/signup").post(signup);
 router.route("/signin").post(signin);
+router.route("/google").post(signinWithGoogle);
 
 export default router;
