fa1409f274dcec00e8f0100e449304b38b21d010
suvam <suvampravu@gmail.com>
2024-08-20 00:56:38 +0530

    update dependencies

--- API/index.js
+++ API/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 //connection import
 import createConnection from "./DB/connect.js";
@@ -14,10 +15,11 @@ import { errorHandler } from "./middlewares/errorHandeler.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //for consume json data
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter);
