327e21f599cf578e1e2c6fef5c3c49531d257cfc
suvam <suvampravu@gmail.com>
2024-08-20 13:01:32 +0530

    little modification for ui

--- client/src/components/utilities/Userprofile.jsx
+++ client/src/components/utilities/Userprofile.jsx
@@ -78,8 +78,8 @@ function Userprofile() {
         </div>
 
         {/* Logout */}
-        <Link
-          className="px-4 py-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-blue-600 dark:hover:text-blue-400 hover:translate-x-2 transition-all duration-300 ease-in-out flex items-center justify-between"
+        <p
+          className="px-4 py-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-blue-600 dark:hover:text-blue-400 hover:translate-x-2 transition-all duration-300 ease-in-out flex items-center justify-between cursor-pointer"
           onClick={() => setIsModalOpen(true)}
         >
           <div className="flex items-center">
@@ -87,7 +87,7 @@ function Userprofile() {
             Logout
           </div>
           <Rightarrow />
-        </Link>
+        </p>
         <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
       </div>
     </>
