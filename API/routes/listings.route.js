b2c435b30ff5cfc085b14ce3250f2679c0a781f5
suvam <suvampravu@gmail.com>
2024-08-20 22:30:14 +0530

    listings route created

--- API/routes/listings.route.js
+++ API/routes/listings.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 
 import { createListings } from "../controllers/listings.controler";
+import { verifyUserToken } from "../utility/verifyUserToken";
 
 const router = express.Router();
 
-router.route("/new").post(createListings);
+router.route("/new").post(verifyUserToken, createListings);
 
 export default router;
