7980861f5c15e2f8191ae4b8453979f1206ad46e
suvam <suvampravu@gmail.com>
2024-08-21 20:21:34 +0530

    added listing route

--- client/src/components/utilities/Userprofile.jsx
+++ client/src/components/utilities/Userprofile.jsx
@@ -50,7 +50,7 @@ function Userprofile() {
 
         {/* Create Listings */}
         <Link
-          to="/account"
+          to="addlistings"
           className="px-4 py-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-blue-600 dark:hover:text-blue-400 hover:translate-x-2 transition-all duration-300 ease-in-out flex items-center justify-between"
         >
           <div className="flex items-center">
