a912cc107acf44c50392b17306f65f04ead5b2e0
suvam <suvampravu@gmail.com>
2024-08-22 21:59:36 +0530

    refactored

--- client/src/components/User-Page/Account.jsx
+++ client/src/components/User-Page/Account.jsx
@@ -23,6 +23,7 @@ import { useDispatch } from "react-redux";
 import Err from "../utilities/Err.jsx";
 import Spin from "../utilities/Spin.jsx";
 import Success from "../utilities/Success.jsx";
+import Progress from "../utilities/Progress.jsx";
 
 function Account({}) {
   const { currUser, loading } = useSelector((state) => state.user);
@@ -157,33 +158,7 @@ function Account({}) {
           </div>
         </div>
         {/* Progress Bar */}
-        {filePrec > 0 && filePrec < 100 && (
-          <div className="mb-4">
-            <div className="relative pt-1">
-              <div className="flex mb-2 items-center justify-between">
-                <div>
-                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
-                    Uploading...
-                  </span>
-                </div>
-                <div className="text-right">
-                  <span className="text-xs font-semibold inline-block text-blue-600">
-                    {filePrec}%
-                  </span>
-                </div>
-              </div>
-              <div className="progress-bar-container mb-4 text-xs flex rounded">
-                <div
-                  className="progress-bar bg-blue-500"
-                  style={{ width: `${filePrec}%` }}
-                ></div>
-              </div>
-              <p className="text-sm text-gray-600 mt-2">
-                Please be patient, changes may take some time to reflect.
-              </p>
-            </div>
-          </div>
-        )}
+        <Progress filePrec={filePrec} />
         {/* <!-- Form --> */}
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           {/* <!-- Name --> */}
