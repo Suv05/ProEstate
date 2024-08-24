d3fee802001c5697b92674bbca4e9326711f430e
suvam <suvampravu@gmail.com>
2024-08-25 01:07:03 +0530

    some changes in connection creation

--- API/DB/connect.js
+++ API/DB/connect.js
@@ -5,17 +5,25 @@ const clientOptions = {
 };
 
 const createConnection = async (uri) => {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
-    throw error; // Re-throw the error to handle it in the app.js
-  }
+  const connectWithRetry = async () => {
+    try {
+      // Attempt to connect to MongoDB
+      await mongoose.connect(uri, clientOptions);
+      // Ping to ensure connection is established
+      await mongoose.connection.db.admin().command({ ping: 1 });
+      console.log(
+        "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+    } catch (error) {
+      console.error("Failed to connect to MongoDB:", error.message);
+      console.log("Retrying connection in 5 seconds...");
+      // Retry connection after a delay
+      setTimeout(connectWithRetry, 5000);
+    }
+  };
+
+  // Initial connection attempt
+  connectWithRetry();
 };
 
 export default createConnection;
