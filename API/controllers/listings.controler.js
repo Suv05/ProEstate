5aa01227e5932bdf6609d9520e73c89cdbd41e9f
suvam <suvampravu@gmail.com>
2024-08-30 12:38:00 +0530

    complete functionality of listings controller

--- API/controllers/listings.controler.js
+++ API/controllers/listings.controler.js
@@ -84,9 +84,7 @@ export const getAllListings = async (req, res, next) => {
   const listings = await Listings.find({}).limit(limit);
 
   if (!listings || listings.length === 0) {
-    return res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ message: "No listings found" });
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: "No listings found" });
   }
 
   return res.status(StatusCodes.OK).json({ listings });
@@ -96,4 +94,55 @@ export const getAllListings = async (req, res, next) => {
 export const getSingleListing = async (req, res, next) => {
   const listing = await Listings.findById(req.params.id);
   return res.status(StatusCodes.OK).json({ listing });
+};
+
+// Handler function to get listings with offer=true and a limit
+export const getOfferListings = async (req, res, next) => {
+  const { offer, limit } = req.query;
+
+  // Query to find listings with offer=true
+  let query = {};
+
+  if (offer === "true") {
+    query.offer = true;
+  }
+
+  // Find listings based on the query and apply the limit
+  const listings = await Listings.find(query).limit(Number(limit));
+
+  if (!listings || listings.length === 0) {
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: "No listings found" });
+  }
+
+  return res.status(StatusCodes.OK).json({ listings });
+};
+
+// Handler function to get listings with sort and a limit
+export const getSortListings = async (req, res, next) => {
+  const limit = parseInt(req.query.limit) || 4; // Default limit to 4 if not provided
+  const listings = await Listings.find({})
+    .sort({ createdAt: -1 }) // Sort by the `createdAt` field in descending order (newest first)
+    .limit(limit);
+
+  if (!listings || listings.length === 0) {
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: "No listings found" });
+  }
+
+  return res.status(StatusCodes.OK).json({ listings });
+};
+
+//sort listings according to the price
+export const getListingsByPrice = async (req, res, next) => {
+  const limit = parseInt(req.query.limit) || 4; // Default limit to 4 if not provided
+  const sortOrder = req.query.sortOrder === "asc" ? 1 : -1; // Sort ascending or descending based on query parameter
+
+  const listings = await Listings.find({})
+    .sort({ regularPrice: sortOrder }) // Sort by the `regularPrice` field
+    .limit(limit);
+
+  if (!listings || listings.length === 0) {
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: "No listings found" });
+  }
+
+  return res.status(StatusCodes.OK).json({ listings });
 };
--- API/routes/listings.route.js
+++ API/routes/listings.route.js
@@ -3,12 +3,30 @@ import {
   searchFunctionality,
   getAllListings,
   getSingleListing,
+  getOfferListings,
+  getSortListings,
+  getListingsByPrice,
 } from "../controllers/listings.controler.js";
 
 const router = express.Router();
 
 // For advanced search functionality
 router.route("/search").get(searchFunctionality);
+
+//to get all listings you can apply limit as well
 router.route("/").get(getAllListings);
+
+//to get listings with offer and limit
+router.route("/offer").get(getOfferListings);
+
+//to get listings with sort and limit
+router.route("/new").get(getSortListings);
+
+//to get listings by sort price and limit
+//api/v1/listings/price?sort=price&limit=4 will work
+//api/v1/listings/price?sort=price&limit=4&sortOrder=asc also work
+router.route("/price").get(getListingsByPrice);
+
+//to get single listing
 router.route("/:id").get(getSingleListing);
 export default router;
