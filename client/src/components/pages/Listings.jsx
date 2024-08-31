36e75f9c89357e61615c01c1616e8968c244e085
suvam <suvampravu@gmail.com>
2024-08-31 11:04:52 +0530

    complete pagination functionality

--- client/src/components/pages/Listings.jsx
+++ client/src/components/pages/Listings.jsx
@@ -5,7 +5,13 @@ import Broken from "../utilities/Broken.jsx";
 import ImageCarousel from "../utilities/ImageCarousel.jsx";
 import { Link } from "react-router-dom";
 
-import { FaCar, FaBed, FaBath } from "react-icons/fa";
+import {
+  FaCar,
+  FaBed,
+  FaBath,
+  FaChevronCircleRight,
+  FaChevronCircleLeft,
+} from "react-icons/fa";
 import { MdOutlinePets } from "react-icons/md";
 
 function Listings({}) {
@@ -24,13 +30,15 @@ function Listings({}) {
   const [err, setErr] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  //pagination state
+  const [currIndex, setCurrIndex] = useState(0);
+  const itemsPerPage = 4;
+
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
         setLoading(true);
-        const response = await fetch(
-          "/api/v1/listings/offer?offer=true&limit=4"
-        );
+        const response = await fetch("/api/v1/listings/offer?offer=true");
         console.log("Offer Listings Response:", response); // Debug response
         if (!response.ok) {
           throw new Error("Failed to fetch offer listings");
@@ -84,6 +92,16 @@ function Listings({}) {
     fetchOfferListings();
   }, []);
 
+  //Navigate to prev page
+  const handlePrev = () => {
+    if (currIndex > 0) setCurrIndex(currIndex - itemsPerPage);
+  };
+  //Navigate to next page
+  const handleNext = () => {
+    if (currIndex + itemsPerPage < offerListings.length)
+      setCurrIndex(currIndex + itemsPerPage);
+  };
+
   if (loading) return <Spinner />;
   if (err) return <Broken />;
 
@@ -120,73 +138,96 @@ function Listings({}) {
             </div>
           </div>
         </div>
+
         {/* offer section  */}
         <div className="bg-white py-16">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-semibold text-gray-800">
               Saved more 💰 On <span className="text-theme">Special Offer</span>
             </h1>
-            <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-              {/* Property cards */}
-              {offerListings.map((listing) => (
-                <Link to={`/listings/${listing._id}`} key={listing._id}>
-                  <div className="bg-white rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
-                    <img
-                      src={listing.imageUrls[0]} // Display the first image
-                      alt={listing.title}
-                      className="w-full h-48 object-cover rounded-t-lg"
-                    />
-                    <div className="p-4">
-                      <div className="flex justify-between items-center">
-                        <h2 className="text-lg font-bold">{listing.title}</h2>
+            <div className="mt-8 flex items-center justify-between">
+              <button
+                onClick={handlePrev}
+                disabled={currIndex === 0}
+                className="text-3xl disabled:text-blue-300 text-blue-600"
+              >
+                <FaChevronCircleLeft />
+              </button>
+
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {/* Displaying only the current set of items */}
+                {offerListings
+                  .slice(currIndex, currIndex + itemsPerPage)
+                  .map((listing) => (
+                    <Link to={`/listings/${listing._id}`} key={listing._id}>
+                      <div className="bg-white rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
+                        <img
+                          src={listing.imageUrls[0]} // Display the first image
+                          alt={listing.title}
+                          className="w-full h-48 object-cover rounded-t-lg"
+                        />
+                        <div className="p-4">
+                          <div className="flex justify-between items-center">
+                            <h2 className="text-lg font-bold">
+                              {listing.title}
+                            </h2>
+                          </div>
+                          <p className="text-sm text-gray-600 mb-4">
+                            {listing.location}
+                          </p>
+                          <div className="flex justify-between items-center mb-4">
+                            <span className="text-blue-600 font-bold text-lg">
+                              ${listing.regularPrice - listing.discountPrice}
+                            </span>
+                            <span
+                              className={`px-2 py-1 text-xs font-semibold rounded ${
+                                listing.offer
+                                  ? "bg-green-100 text-green-800"
+                                  : "bg-gray-100 text-gray-800"
+                              }`}
+                            >
+                              {listing.offer ? "Special Offer" : "Regular"}
+                            </span>
+                          </div>
+                          <div className="flex justify-between text-sm text-gray-600">
+                            <span className="flex items-center space-x-2 hover:text-blue-600 transition duration-300 ease-in-out transform hover:scale-105">
+                              <FaBed />
+                              <span>{listing.bedrooms} Beds</span>
+                            </span>
+                            <span className="flex items-center space-x-2 hover:text-blue-600 transition duration-300 ease-in-out transform hover:scale-105">
+                              <FaBath />
+                              <span>{listing.bathrooms} Baths</span>
+                            </span>
+                            <span className="flex items-center space-x-2 hover:text-blue-600 transition duration-300 ease-in-out transform hover:scale-105">
+                              {listing.parking ? (
+                                <FaCar />
+                              ) : listing.petsAllowed ? (
+                                <MdOutlinePets />
+                              ) : (
+                                <FaCar />
+                              )}
+                              <span>
+                                {listing.parking
+                                  ? "Parking"
+                                  : listing.petsAllowed
+                                  ? "Pets Allowed"
+                                  : "No Parking"}
+                              </span>
+                            </span>
+                          </div>
+                        </div>
                       </div>
-                      <p className="text-sm text-gray-600 mb-4">
-                        {listing.location}
-                      </p>
-                      <div className="flex justify-between items-center mb-4">
-                        <span className="text-blue-600 font-bold text-lg">
-                          ${listing.regularPrice - listing.discountPrice}
-                        </span>
-                        <span
-                          className={`px-2 py-1 text-xs font-semibold rounded ${
-                            listing.offer
-                              ? "bg-green-100 text-green-800"
-                              : "bg-gray-100 text-gray-800"
-                          }`}
-                        >
-                          {listing.offer ? "Special Offer" : "Regular"}
-                        </span>
-                      </div>
-                      <div className="flex justify-between text-sm text-gray-600">
-                        <span className="flex items-center space-x-2 hover:text-blue-600 transition duration-300 ease-in-out transform hover:scale-105">
-                          <FaBed />
-                          <span>{listing.bedrooms} Beds</span>
-                        </span>
-                        <span className="flex items-center space-x-2 hover:text-blue-600 transition duration-300 ease-in-out transform hover:scale-105">
-                          <FaBath />
-                          <span>{listing.bathrooms} Baths</span>
-                        </span>
-                        <span className="flex items-center space-x-2 hover:text-blue-600 transition duration-300 ease-in-out transform hover:scale-105">
-                          {listing.parking ? (
-                            <FaCar />
-                          ) : listing.petsAllowed ? (
-                            <MdOutlinePets />
-                          ) : (
-                            <FaCar />
-                          )}
-                          <span>
-                            {listing.parking
-                              ? "Parking"
-                              : listing.petsAllowed
-                              ? "Pets Allowed"
-                              : "No Parking"}
-                          </span>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              ))}
+                    </Link>
+                  ))}
+              </div>
+
+              <button
+                onClick={handleNext}
+                disabled={currIndex + itemsPerPage >= offerListings.length}
+                className="text-3xl text-blue-600 disabled:text-blue-300"
+              >
+                <FaChevronCircleRight />
+              </button>
             </div>
           </div>
         </div>
